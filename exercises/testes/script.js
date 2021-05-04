47815fee8ef6092d4c008d428e1bfe1bc80a64df
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-03 21:23:09 -0300

    adiciona todos exercicios novos feitos

---
+++ exercises/testes/script.js
@@ -0,0 +1,10 @@
+let num = [2,5,2,6,8,1,8];
+let maior = 8;
+
+function buscaRepetidos(valor) {
+    if (valor === maior)
+    return valor
+}
+
+let numerosRepetidos = num.filter(buscaRepetidos);
+console.log(numerosRepetidos);
