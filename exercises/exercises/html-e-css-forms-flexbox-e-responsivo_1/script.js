a87c0dc6f0a58e23e0c293cecd8a8e01747e57b2
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-11 19:11:42 -0300

    Exercicio 1

---
+++ exercises/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,13 @@
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+let estadoSelect = document.getElementById('estado');
+
+for (let index = 0; index < estados.length; index += 1) {
+    let estadoOption = estados[index];
+    
+    let option = document.createElement('option');
+    option.innerText = estadoOption;
+
+    estadoSelect.appendChild(option);
+}
+
